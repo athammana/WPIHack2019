4ab7f8e73d55decf83c400322af2beb77aafec2b
athammana <aj@virpietech.com>
2019-01-18 23:21:31 -0500

    Task and Routine

---
+++ app/src/main/java/com/example/wpihack2019/Routine.java
@@ -0,0 +1,24 @@
+package com.example.wpihack2019;
+
+public class Routine {
+
+    String Name;
+    int priority;
+    int order;
+    int minHours;
+    int minMins;
+    int maxHours;
+    int maxMins;
+
+    public Routine(String Name, int priority, int order, int minHours, int minMins, int maxHours, int maxMins){
+        Name = this.Name;
+        priority = this.priority;
+        order = this.order;
+        minHours = this.minHours;
+        minMins = this.minMins;
+        maxHours = this.maxHours;
+        maxMins = this.maxMins;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/wpihack2019/Task.java
@@ -0,0 +1,25 @@
+package com.example.wpihack2019;
+
+public class Task {
+
+    String Name;
+    int priority;
+    int order;
+    int minHours;
+    int minMins;
+    int maxHours;
+    int maxMins;
+
+    public Task(String Name, int priority, int order, int minHours, int minMins, int maxHours, int maxMins){
+        Name = this.Name;
+        priority = this.priority;
+        order = this.order;
+        minHours = this.minHours;
+        minMins = this.minMins;
+        maxHours = this.maxHours;
+        maxMins = this.maxMins;
+    }
+
+
+
+}
